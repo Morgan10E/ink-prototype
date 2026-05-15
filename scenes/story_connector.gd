aa0abae58d270be9982ddbbe5c4fe5f15a3775b0
Morgan Tenney <morgan.tenney@gmail.com>
2026-05-14 20:18:46 -0700

    wip storymanager, level navigation

--- scenes/story_connector.gd
+++ scenes/story_connector.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal open_ink_story(entrypoint: String)
+
 var last_ink_entrypoint: String = ""
 
 func update_ink_entrypoint(new_entrypoint: String):
@@ -9,5 +11,6 @@ func trigger_story():
     if last_ink_entrypoint != "":
         print("Open the Ink story now!")
         print("Active story: ", last_ink_entrypoint)
+        emit_signal("open_ink_story", last_ink_entrypoint)
     else:
         print("no-op: no story active")
