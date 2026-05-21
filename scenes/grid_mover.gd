7881e064ea691890c1e1e156370624f28e546834
Morgan Tenney <morgan.tenney@gmail.com>
2026-05-20 20:07:58 -0700

    fine-tune movement; story connector handles getting the ink entrypoint

--- scenes/grid_mover.gd
+++ scenes/grid_mover.gd
@@ -2,23 +2,45 @@ extends Node2D
 
 @export var move_object: Node2D
 @export var speed = 0.25
+@export var time_til_stopped: float = 1
+@export var time_til_momentum: float = 1
 @export var raycast: RayCast2D
 
 var grid_position: Vector2 = Vector2.ZERO
 var moving_direction: Vector2 = Vector2.ZERO
 var facing_direction: Vector2 = Vector2.DOWN
 
-signal facing_ink_entrypoint(entrypoint: String)
+var has_momentum: bool = false
+var movement_attempted: bool = false
+var slowdown_timer: float = 0
+var speedup_timer: float = 0
+
+signal move_done
+
+func _process(delta: float) -> void:
+    slowdown_timer += delta
+    speedup_timer += delta
+    if movement_attempted:
+        slowdown_timer = 0
+    elif slowdown_timer > time_til_stopped:
+        has_momentum = false
+    
+    if not movement_attempted:
+        speedup_timer = 0
+    elif speedup_timer > time_til_momentum:
+        has_momentum = true
+    
+    movement_attempted = false
 
 func move(direction: Vector2):
+    movement_attempted = true
     if not is_moving() and direction.length() > 0:
         var new_facing_direction = normalize_direction(direction)
         if facing_direction != new_facing_direction:
             # rotate in place if facing a new direction
             facing_direction = new_facing_direction
             update_direction()
-            emit_signal("facing_ink_entrypoint", get_ink_entrypoint())
-        else:
+        elif has_momentum:
             # if already facing this direction, move
             move_forward()
 
@@ -37,7 +59,7 @@ func move_forward() -> void:
 
 func finish_move() -> void:
     moving_direction = Vector2.ZERO
-    emit_signal("facing_ink_entrypoint", get_ink_entrypoint())
+    emit_signal("move_done")
 
 func is_moving() -> bool:
     return moving_direction.length() > 0
@@ -63,15 +85,4 @@ func get_coordinates_from_vector2(int_coords: Vector2):
 func update_direction():
     raycast.target_position = facing_direction * Constants.TILE_SIZE
     raycast.force_raycast_update()
-
-func get_ink_entrypoint():
-    var colliding_with = raycast.get_collider()
-    if colliding_with != null:
-        if colliding_with is TileMapLayer:
-            var raycast_target = raycast.to_global(raycast.target_position)
-            var cell = colliding_with.local_to_map(raycast_target)
-            var tile_data: TileData = colliding_with.get_cell_tile_data(cell)
-            if tile_data:
-                var value = tile_data.get_custom_data("ink_entrypoint")
-                return value
-    return ""
+    emit_signal("move_done")
--- scenes/story_connector.gd
+++ scenes/story_connector.gd
@@ -1,8 +1,22 @@
 extends Node
 
+@export var raycast: RayCast2D
+
 var last_ink_entrypoint: String = ""
 
-func update_ink_entrypoint(new_entrypoint: String):
+func update_ink_entrypoint():
+    last_ink_entrypoint = ""
+    var colliding_with = raycast.get_collider()
+    if colliding_with != null:
+        if colliding_with is TileMapLayer:
+            var raycast_target = raycast.to_global(raycast.target_position)
+            var cell = colliding_with.local_to_map(raycast_target)
+            var tile_data: TileData = colliding_with.get_cell_tile_data(cell)
+            if tile_data:
+                var value = tile_data.get_custom_data("ink_entrypoint")
+                last_ink_entrypoint = value
+
+func set_ink_entrypoint(new_entrypoint: String):
     last_ink_entrypoint = new_entrypoint
 
 func trigger_story():
